9417f813a30bb723fdca89a7073299f9998ccb88
Cavon <lijiarong@sunfounder.com>
2017-08-23 18:39:56 +0800

    fix project saving

--- Dragit/static/snap/gui.js
+++ Dragit/static/snap/gui.js
@@ -4557,7 +4557,7 @@ IDE_Morph.prototype.toggleAppMode = function (appMode) {
     var world = this.world(),
         elements = [
             this.logo,
-            this.controlBar.cloudButton,
+            //this.controlBar.cloudButton,
             this.controlBar.projectButton,
             this.controlBar.settingsButton,
             this.controlBar.stageSizeButton,
