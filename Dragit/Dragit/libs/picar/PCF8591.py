cc298b09414e75256bdf4d7eb7acdee8ef1f1970
Cavon <cavonxx@gmail.com>
2018-11-12 13:57:22 +0800

    update

--- Dragit/Dragit/libs/picar/PCF8591.py
+++ Dragit/Dragit/libs/picar/PCF8591.py
@@ -15,92 +15,92 @@ import time
 
 
 class PCF8591(object):
-	""" Light_Follow Module class """
-	RPI_REVISION_0 = ["900092"]
-	RPI_REVISION_1_MODULE_B = ["Beta", "0002", "0003", "0004", "0005", "0006", "000d", "000e", "000f"]
-	RPI_REVISION_1_MODULE_A = ["0007", "0008", "0009",]
-	RPI_REVISION_1_MODULE_BP = ["0010", "0013"]
-	RPI_REVISION_1_MODULE_AP = ["0012"]
-	RPI_REVISION_2_MODULE_B = ["a01041", "a21041"]
-	RPI_REVISION_3_MODULE_B = ["a02082", "a22082"]
+    """ Light_Follow Module class """
+    RPI_REVISION_0 = ["900092"]
+    RPI_REVISION_1_MODULE_B = ["Beta", "0002", "0003", "0004", "0005", "0006", "000d", "000e", "000f"]
+    RPI_REVISION_1_MODULE_A = ["0007", "0008", "0009",]
+    RPI_REVISION_1_MODULE_BP = ["0010", "0013"]
+    RPI_REVISION_1_MODULE_AP = ["0012"]
+    RPI_REVISION_2_MODULE_B = ["a01041", "a21041"]
+    RPI_REVISION_3_MODULE_B = ["a02082", "a22082"]
     RPI_REVISION_3_MODULE_BP = ["a020d3"]
 
-	AD_CHANNEL = [0x43, 0x42, 0x41, 0x40]
+    AD_CHANNEL = [0x43, 0x42, 0x41, 0x40]
 
-	def __init__(self, address=0x48, bus_number=1):
-		self.address = address
-		if bus_number == None:
-			self._bus_number = self._get_bus_number()
-		else:
-			self._bus_number = bus_number
-		self.bus = smbus.SMBus(self._bus_number)
+    def __init__(self, address=0x48, bus_number=1):
+        self.address = address
+        if bus_number == None:
+            self._bus_number = self._get_bus_number()
+        else:
+            self._bus_number = bus_number
+        self.bus = smbus.SMBus(self._bus_number)
 
-	def read(self, chn): #channel
-		self.bus.write_byte(self.address, self.AD_CHANNEL[chn])
-		self.bus.read_byte(self.address) # dummy read to start conversion
-		return self.bus.read_byte(self.address)
+    def read(self, chn): #channel
+        self.bus.write_byte(self.address, self.AD_CHANNEL[chn])
+        self.bus.read_byte(self.address) # dummy read to start conversion
+        return self.bus.read_byte(self.address)
 
-	@property
-	def A0(self):
-		return self.read(0)
-	@property
-	def A1(self):
-		return self.read(1)
-	@property
-	def A2(self):
-		return self.read(2)
-	@property
-	def A3(self):
-		return self.read(3)
+    @property
+    def A0(self):
+        return self.read(0)
+    @property
+    def A1(self):
+        return self.read(1)
+    @property
+    def A2(self):
+        return self.read(2)
+    @property
+    def A3(self):
+        return self.read(3)
 
-	def _get_bus_number(self):
-		"Gets the version number of the Raspberry Pi board"
-		# Courtesy quick2wire-python-api
-		# https://github.com/quick2wire/quick2wire-python-api
-		# Updated revision info from: http://elinux.org/RPi_HardwareHistory#Board_Revision_History
-		try:
-			f = open('/proc/cpuinfo','r')
-			for line in f:
-				if line.startswith('Revision'):
-					if line[11:-1] in self.RPI_REVISION_0:
-						return 0
-					elif line[11:-1] in self.RPI_REVISION_1_MODULE_B:
-						return 0
-					elif line[11:-1] in self.RPI_REVISION_1_MODULE_A:
-						return 0
-					elif line[11:-1] in self.RPI_REVISION_1_MODULE_BP:
-						return 1
-					elif line[11:-1] in self.RPI_REVISION_1_MODULE_AP:
-						return 0
-					elif line[11:-1] in self.RPI_REVISION_2_MODULE_B:
-						return 1
-					elif line[11:-1] in self.RPI_REVISION_3_MODULE_B:
-						return 1
-					elif line[11:-1] in self.RPI_REVISION_3_MODULE_BP:
-						return 1
-					else:
-						return line[11:-1]
-		except:
-			f.close()
-			return 'Open file error'
-		finally:
-			f.close()
+    def _get_bus_number(self):
+        "Gets the version number of the Raspberry Pi board"
+        # Courtesy quick2wire-python-api
+        # https://github.com/quick2wire/quick2wire-python-api
+        # Updated revision info from: http://elinux.org/RPi_HardwareHistory#Board_Revision_History
+        try:
+            f = open('/proc/cpuinfo','r')
+            for line in f:
+                if line.startswith('Revision'):
+                    if line[11:-1] in self.RPI_REVISION_0:
+                        return 0
+                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_B:
+                        return 0
+                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_A:
+                        return 0
+                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_BP:
+                        return 1
+                    elif line[11:-1] in self.RPI_REVISION_1_MODULE_AP:
+                        return 0
+                    elif line[11:-1] in self.RPI_REVISION_2_MODULE_B:
+                        return 1
+                    elif line[11:-1] in self.RPI_REVISION_3_MODULE_B:
+                        return 1
+                    elif line[11:-1] in self.RPI_REVISION_3_MODULE_BP:
+                        return 1
+                    else:
+                        return line[11:-1]
+        except:
+            f.close()
+            return 'Open file error'
+        finally:
+            f.close()
 
 def test():
-	ADC = PCF8591(0x48)
-	while True:
-		A0 = ADC.read(0)
-		A1 = ADC.read(1)
-		A2 = ADC.read(2)
+    ADC = PCF8591(0x48)
+    while True:
+        A0 = ADC.read(0)
+        A1 = ADC.read(1)
+        A2 = ADC.read(2)
 
-		print "A0 = %d  A1 = %d  A2 = %d"%(A0,A1,A2)
-		time.sleep(0.5)
+        print "A0 = %d  A1 = %d  A2 = %d"%(A0,A1,A2)
+        time.sleep(0.5)
 
 def destroy():
-	pass
+    pass
 
 if __name__ == '__main__':
-	try:
-		test()
-	except KeyboardInterrupt:
-		destroy()
+    try:
+        test()
+    except KeyboardInterrupt:
+        destroy()
